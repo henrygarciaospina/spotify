e12d31a7d17934aca72bcdbb7f51779d3d6eac64
osboxes.org <osboxes.henrygarcia0309@gmail.com>
2017-05-07 22:23:47 -0500

    Creación de modelos Album, Artist, Song y sus relaciones

---
+++ db/migrate/20170508014957_add_album_to_artists.rb
@@ -0,0 +1,5 @@
+class AddAlbumToArtists < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :artists, :album, foreign_key: true
+  end
+end
---
+++ db/migrate/20170508015300_add_song_to_albums.rb
@@ -0,0 +1,5 @@
+class AddSongToAlbums < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :albums, :song, foreign_key: true
+  end
+end
